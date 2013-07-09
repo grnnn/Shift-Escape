165cfd8bbb4f1b2039f9c9e9f519b618d5f6c4e5
Taylor Owen <towenmil@ucsc.edu>
2013-07-09 02:11:56 -0700

    got shift working

--- Grid/Grid.js
+++ Grid/Grid.js
@@ -35,6 +35,8 @@ var Grid = function(size, scene){
 	
 	this.selector = new Selector(0, 0, this.scene);
 	
+	this.shiftType = "h";
+	
 	this.init();
 	
 }
@@ -59,4 +61,104 @@ Grid.prototype.init = function(){
 
 Grid.prototype.fillTile = function(x, y){
 	this.array[x-1][y-1].fill();
+}
+
+Grid.prototype.horizontalShift = function(){
+	this.shiftType = "horizontal";
+	
+	var vertPos = this.selector.y + 1;
+	
+	var temp = this.array[0][vertPos];
+	
+	for(var i = 0; i < this.size - 1; i++){
+		this.array[i][vertPos] = this.array[i+1][vertPos];
+	}
+	this.array[this.size-1][vertPos] = temp;
+}
+
+Grid.prototype.verticalShift = function(){
+	this.shiftType = "vertical";
+	
+	var horPos = this.selector.x + 1;
+	
+	var temp = this.array[horPos][0];
+	
+	for(var i = 0; i < this.size - 1; i++){
+		this.array[horPos][i] = this.array[horPos][i+1];
+	}
+	this.array[horPos][this.size - 1] = temp;
+}
+
+Grid.prototype.animate = function(){
+	var bool = true;
+	
+	if(this.shiftType === "horizontal") bool = this.animateh();
+	if(this.shiftType === "vertical") bool = this.animatev(); 
+	
+	return bool;
+}
+
+Grid.prototype.tracker = 55;
+Grid.prototype.aToggle = false;
+
+Grid.prototype.animateh = function(){
+	
+	var vertPos = this.selector.y + 1;
+	
+	
+	if(this.tracker !== 0){
+		for(var i = 0; i < this.size; i++){
+			this.array[i][vertPos].cube.position.x -= 1;	
+			
+		}
+		this.tracker -= 1;
+	}
+	
+	if(this.tracker === 0){
+		this.array[this.size-1][vertPos].cube.position.y -= 10;
+	}
+	
+	if(this.array[this.size-1][vertPos].cube.position.y < -500){
+		this.array[this.size-1][vertPos].cube.position.y = 500;
+		this.array[this.size-1][vertPos].cube.position.x = 55*(this.size-1);
+		this.aToggle = true;
+	}
+	
+	if(this.aToggle && this.array[this.size-1][vertPos].cube.position.y === 0){
+		this.tracker = 55;
+		this.aToggle = false;
+		return false;
+	}
+	
+	return true;
+}
+
+Grid.prototype.animatev = function(){
+	var horPos = this.selector.x + 1;
+	
+	
+	if(this.tracker !== 0){
+		for(var i = 0; i < this.size; i++){
+			this.array[horPos][i].cube.position.z -= 1;
+		}
+		this.tracker -= 1;
+	}
+	
+	if(this.tracker === 0){
+		this.array[horPos][this.size-1].cube.position.y -= 10;
+	}
+	
+	if(this.array[horPos][this.size-1].cube.position.y < -500){
+		this.array[horPos][this.size-1].cube.position.y = 500;
+		this.array[horPos][this.size-1].cube.position.z = 55*(this.size-1);
+		this.aToggle = true;
+	}
+	
+	if(this.aToggle && this.array[horPos][this.size-1].cube.position.y === 0){
+		this.tracker = 55;
+		this.aToggle = false;
+		return false;
+	}
+	
+	return true;
 }
--- Player/Player.js
+++ Player/Player.js
@@ -1,6 +1,6 @@
 var Selector = function(x, y, scene){
-	this.x = x;
-	this.y = y;
+	this.x = (x-1);
+	this.y = (y-1);
 	
 	this.cube = new THREE.Mesh(
         		new THREE.CubeGeometry(53, 18, 53),
@@ -17,16 +17,36 @@ var Selector = function(x, y, scene){
 
 Selector.prototype.shiftUp = function(){
 	this.cube.position.z += 55;
+	this.y++;
 }
 
 Selector.prototype.shiftDown = function(){
 	this.cube.position.z -= 55;
+	this.y--;
 }
 
 Selector.prototype.shiftLeft = function(){
 	this.cube.position.x += 55;
+	this.x++;
 }
 
 Selector.prototype.shiftRight = function(){
 	this.cube.position.x -= 55;
+	this.x--;
+}
+
+Selector.prototype.canMove = function(dir){
+	if(dir === "up"){
+		if(this.y > 5) return false;
+	}
+	if(dir === "down"){
+		if(this.y < 0) return false;
+	}
+	if(dir === "left"){
+		if(this.x > 5) return false;
+	}
+	if(dir === "right"){
+		if(this.x < 0) return false;
+	}
+	return true;
 }
